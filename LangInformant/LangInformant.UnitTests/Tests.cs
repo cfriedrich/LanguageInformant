82f8b05016f6c7cdf47acb356ab0c6993641ae25
madmisfit <madmisfit@gmail.com>
2014-05-01 00:02:06 -0700

    Added a CreateWord Unit test

--- LangInformant/LangInformant.UnitTests/Tests.cs
+++ LangInformant/LangInformant.UnitTests/Tests.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LangInformant.Domain.Entities;
 
 namespace LangInformant.UnitTests
 {
     [TestClass]
-    public class Tests
+    public class RepositoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void createWordTest()
         {
+            Word target = new Word();
+            const string NAME = "Testing";
+            const string DESCRIPTION = "Test text.";
+            const string LANGUAGE = "Spanish";
+            const int SOUNDVOL = 4;
+            Language testLang = new Language();
+
+
+            target.Name = NAME;
+            target.Description = DESCRIPTION;
+            target.SoundVol = SOUNDVOL;
+            testLang.Name = LANGUAGE;
+
+            Assert.AreEqual(NAME, target.Name);
+            Assert.AreEqual(DESCRIPTION, target.Description);
+            Assert.AreEqual(SOUNDVOL, target.SoundVol);
+            Assert.AreEqual(LANGUAGE, testLang.Name);
         }
     }
 }
