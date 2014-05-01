5520035ffd77d3a0495cd1eafc691d5e136fd110
cfriedrich <chrisfriedrich@outlook.com>
2014-04-30 18:47:32 -0700

    Incorporated User Changes

---
+++ LangInformant/LangInformant.WebUI/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LangInformant.Domain.Entities;
+using LangInformant.Domain.Concrete;
+
+namespace LangInformant.WebUI.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User info)
+        {
+            //ToDO
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
